f506cffe1139748f4312b8a512b1748e32d6d477
ceyahya <113285702+ceyahya@users.noreply.github.com>
2022-10-13 13:25:07 +0200

    update

--- js/app.js
+++ js/app.js
@@ -14,40 +14,40 @@ for (let index = 1; index <= sections.length; index++) {
     document.getElementById("navbar__list").appendChild(nav);
 }
 // end the navigatinbar icons 
-// button to build more section and its navigationbar icon
-btn.onclick = function () {
-    counter += 1;
-// build the navigatinbar icon
-    let nav = document.createElement("li");
-    let link = document.createElement("a");
-    link.href = `#section${counter}`;
-    link.id = `#section${counter}`;
-    let linkTxt = document.createTextNode(`Section ${counter}`);
-    link.appendChild(linkTxt);
-    nav.appendChild(link);
-    document.getElementById("navbar__list").appendChild(nav);
-// build the section
-    let sec = document.createElement("section");
-    sec.id = `section${counter}`;
-    sec.setAttribute("data-nav", `Section ${counter}`);
-    let div = document.createElement("div");
-    div.className = "landing__container";
-    sec.appendChild(div);
-    let h2 = document.createElement("h2");
-    let h2Txt = document.createTextNode(`Section ${counter}`);
-    h2.appendChild(h2Txt);
-    div.appendChild(h2);
-    let p1 = document.createElement("p");
-    let p1Txt = document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.");
-    p1.appendChild(p1Txt);
-    div.appendChild(p1);
-    let p2 = document.createElement("p");
-    let p2Txt = document.createTextNode("Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.");
-    p2.appendChild(p2Txt);
-    div.appendChild(p2);
-    document.getElementById("father").appendChild(sec);
-}
-// end button function
+// // button to build more section and its navigationbar icon
+// btn.onclick = function () {
+//     counter += 1;
+// // build the navigatinbar icon
+//     let nav = document.createElement("li");
+//     let link = document.createElement("a");
+//     link.href = `#section${counter}`;
+//     link.id = `#section${counter}`;
+//     let linkTxt = document.createTextNode(`Section ${counter}`);
+//     link.appendChild(linkTxt);
+//     nav.appendChild(link);
+//     document.getElementById("navbar__list").appendChild(nav);
+// // build the section
+//     let sec = document.createElement("section");
+//     sec.id = `section${counter}`;
+//     sec.setAttribute("data-nav", `Section ${counter}`);
+//     let div = document.createElement("div");
+//     div.className = "landing__container";
+//     sec.appendChild(div);
+//     let h2 = document.createElement("h2");
+//     let h2Txt = document.createTextNode(`Section ${counter}`);
+//     h2.appendChild(h2Txt);
+//     div.appendChild(h2);
+//     let p1 = document.createElement("p");
+//     let p1Txt = document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.");
+//     p1.appendChild(p1Txt);
+//     div.appendChild(p1);
+//     let p2 = document.createElement("p");
+//     let p2Txt = document.createTextNode("Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.");
+//     p2.appendChild(p2Txt);
+//     div.appendChild(p2);
+//     document.getElementById("father").appendChild(sec);
+// }
+// // end button function
 // active class
 // the main function to scroll position
 window.onscroll = function () {
@@ -69,18 +69,19 @@ window.onscroll = function () {
 // remove active class function
 let removeActive = function () {
     sections.forEach((Element) => {
-        Element.classList.remove("active")
+        Element.classList.remove("active");
+        document.getElementById(`#${Element.attributes.id.value}`).classList.remove("active");
     })
 }
 // add active class function
 let addActive = function (id) {
-    let selector = document.querySelector(`#${id}`)
-    selector.classList.add("active")
+    document.getElementById(`${id}`).classList.add("active")
+    document.getElementById(`#${id}`).classList.add("active")
 }
 // end active class
 // smooth scroll
 // variable the anchors
-let links = document.getElementsByTagName("a")
+let links = document.querySelectorAll("a")
 for (const link of links) {
     link.addEventListener("click", (event) => {
         event.preventDefault()
